b1be1eb57ac9e3c3b7be6b536862b6dcf28284b1
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-03-28 23:46:31 +0530

    image in product added

--- frontend/app/product/page.jsx
+++ frontend/app/product/page.jsx
@@ -7,6 +7,7 @@ const Product = () => {
   const [product, setProduct] = useState({
     name: "",
     price: null,
+    image: "",
   });
   const changeProduct = (e) => {
     setProduct({
@@ -62,6 +63,25 @@ const Product = () => {
             onChange={changeProduct}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Enter price..."
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="Price"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Image
+          </label>
+          <input
+            type="text"
+            id="image"
+            name="image"
+            value={product.image}
+            onChange={changeProduct}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Enter image url..."
             required
           />
         </div>
